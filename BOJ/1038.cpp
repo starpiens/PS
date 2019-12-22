1dbfaf35341635627ef36a2adf02f454636f18b0
Jihun Kim <starlettkim@gmail.com>
2019-12-23 00:43:55 +0900

    1038번: 감소하는 수

--- BOJ/1038.cpp
+++ BOJ/1038.cpp
@@ -1,8 +1,41 @@
-#include <stdio.h>
-int dp[7][10];
+#include <cstdio>
+#include <numeric>
+
+using namespace std;
+
+int dp[11][11];
+int sum[11][11];
+
 int main() {
-    for (int i = 0; i <= 5; i++)
-        for (int j = 1; j <= 9; j++) {
-            dp[i][j]
+    int N;
+    scanf("%d", &N);
+
+    for (int i = 1; i <= 10; i++) {
+        dp[i][i - 1] = 1;
+        for (int j = i; j < 10; j++)
+            dp[i][j] = dp[i][j - 1] + dp[i - 1][j - 1];
+    }
+    for (int i = 1; i <= 10; i++) {
+        sum[i][i - 1] = 0;
+        for (int j = i; j <= 10; j++)
+            sum[i][j] = sum[i][j - 1] + dp[i][j - 1];
+    }
+
+    for (int i = 1; i <= 10; i++) {
+        if (N < sum[i][10]) {
+            for (int j = i; j > 0; j--) {
+                for (int k = j - 1; k < 10; k++)
+                    if (N < sum[j][k + 1]) {
+                        printf("%d", k);
+                        N -= sum[j][k];
+                        break;
+                    }
+            }
+            return 0;
         }
+        N -= sum[i][10];
+    }
+
+    printf("-1");
+    return 0;
 }
