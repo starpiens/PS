190d220004078199593b9e497422811527551212
Jihun Kim <starlettkim@gmail.com>
2020-01-19 19:44:10 +0900

    12100번 최적화

--- BOJ/12100.cpp
+++ BOJ/12100.cpp
@@ -9,12 +9,13 @@ int mx;
 int dr[] = {1, 0, -1, 0};
 int dc[] = {0, 1, 0, -1};
 
-void dfs(int n, int b[][20]) {
+void dfs(int n, int b[][20], int prev_d) {
     if (n == 5) return;
     for (int i = 0; i < 4; i++) {
+        if (i == prev_d) continue;
         int nxt_b[20][20];
         for (int j = 0; j < N; j++) {
-            int anchor = (dr[i] == 1 || dc[i] == 1) * (N + 1) - 1;
+            int anchor = (dr[i] == 1 || dc[i] == 1) * (N - 1);
             for (int k = 0; k < N; k++) {
                 int r, c;
                 if (dr[i] + dc[i] > 0) r = N - 1 - k, c = j;
@@ -22,22 +23,24 @@ void dfs(int n, int b[][20]) {
                 if (!dr[i]) swap(r, c);
 
                 nxt_b[r][c] = b[r][c];
-                int nxt_r = r + dr[i], nxt_c = c + dc[i];
-                while ((dr[i] ? nxt_r : nxt_c) != anchor && !nxt_b[nxt_r][nxt_c]) {
-                    swap(nxt_b[nxt_r][nxt_c], nxt_b[r][c]);
-                    nxt_r = (r = nxt_r) + dr[i];
-                    nxt_c = (c = nxt_c) + dc[i];
-                }
-                if ((dr[i] ? nxt_r : nxt_c) != anchor && nxt_b[nxt_r][nxt_c] == nxt_b[r][c]) {
+                if (!b[r][c]) continue;
+                mx = max(mx, b[r][c]);
+                int &ch = dr[i] ? nxt_b[anchor][c] : nxt_b[r][anchor];
+                if ((dr[i] ? r : c) == anchor) continue;
+                if (ch == nxt_b[r][c]) {
                     nxt_b[r][c] = 0;
-                    r = nxt_r, c = nxt_c;
-                    nxt_b[r][c] <<= 1;
-                    anchor = (dr[i] ? r : c);
+                    ch <<= 1;
+                    mx = max(mx, ch);
+                    anchor -= (dr[i] + dc[i]);
+                } else if (!ch) {
+                    swap(nxt_b[r][c], ch);
+                } else {
+                    anchor -= (dr[i] + dc[i]);
+                    swap(dr[i] ? nxt_b[anchor][c] : nxt_b[r][anchor], nxt_b[r][c]);
                 }
-                mx = max(mx, nxt_b[r][c]);
             }
         }
-        dfs(n + 1, nxt_b);
+        dfs(n + 1, nxt_b, i);
     }
 }
 
@@ -46,7 +49,7 @@ int main() {
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
             scanf("%d", &board[i][j]);
-    dfs(0, board);
+    dfs(0, board, -1);
     printf("%d", mx);
     return 0;
 }
