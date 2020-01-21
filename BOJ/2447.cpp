65d4ccb60412b40dd4fae8a83b807a30ed87ea8a
Jihun Kim <starlettkim@gmail.com>
2020-01-21 20:29:41 +0900

    Update 2447

--- BOJ/2447.cpp
+++ BOJ/2447.cpp
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-char arr[6561][6361];
+char in[6561][6361];
 
 void f(int r, int c, int s) {
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
             if (i != 1 || j != 1) {
-                if (s == 3) arr[r + i][c + j] = 1;
+                if (s == 3) in[r + i][c + j] = 1;
                 else f(r + i * s / 3, c + j * s / 3, s / 3);
             }
 }
@@ -18,6 +18,6 @@ int main() {
     f(0, 0, N);
     for (int i = 0; i < N; i++, printf("\n"))
         for (int j = 0; j < N; j++)
-            printf("%c", arr[i][j] ? '*' : ' ');
+            printf("%c", in[i][j] ? '*' : ' ');
     return 0;
 }
