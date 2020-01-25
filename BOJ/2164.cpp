429eb295d241e97f88bfa362f5b13a7aa513e3e0
Jihun Kim <starlettkim@gmail.com>
2020-01-25 22:19:01 +0900

    2164번: 카드2

---
+++ BOJ/2164.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <queue>
+using namespace std;
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    queue<int> q;
+    for (int i = 1; i <= N; i++)
+        q.push(i);
+    while (q.size() > 1) {
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    printf("%d", q.front());
+    return 0;
+}
